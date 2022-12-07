b2fb7bc24bf0dce3a2c669f1ba1e366eb555b681
Siraphob <kopinw555@gmail.com>
2022-12-07 09:41:45 +0700

    uploading world_time folder

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Flutter_worldTime
+This is a flutter tutorial application that I learnt from NetNinja: https://netninja.dev/
---
+++ lib/pages/loading.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Chicago', flag: 'usa.png', url: 'America/Chicago');
+    await instance.getTime();
+    if (!mounted) return;
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDaytime': instance.isDaytime,
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+          child: SpinKitRing(
+            color: Colors.white,
+            size: 50.0,
+          )
+      )
+    );
+  }
+}
